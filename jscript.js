4ad53c5dcc6525d504b3711b5ce2ef305ce8c550
unknown <Administrator@V-DUCUMB-SB.redmond.corp.microsoft.com>
2018-09-21 11:28:59 -0700

    uni button fix x2

--- jscript.js
+++ jscript.js
@@ -51,9 +51,9 @@ function webToastButton(nTitle, nBody, nIcon, nImage)
     }
 }
 
-function uniButton(isApp = document.URL.indexOf('http') !== 0) 
+function uniButton(isApp = document.URL.indexOf('http')) 
 {
-    if (isApp = 0) {
+    if (isApp = 1) {
         appToastButton()
     } else {
         webToastButton("Hello There!", "[Sample Body Text]","", "https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&h=350")
